f4723a85d6aae144e35b961b8e2608e1e1fb1a46
win <binomossss@protonmail.com>
2024-11-25 16:17:57 +0200

    fix

--- main.py
+++ main.py
@@ -21,26 +21,26 @@ logger = logging.getLogger(__name__)
 RegisterTelegramFunctions()
 
 
-try:
-    logger.info('Бот запущен')
-    bot.infinity_polling(skip_pending=True)
-except KeyboardInterrupt:
-    logger.info('Выход из программы')
-finally:
-    logger.warning('Программа остановлена')
-    redis_cache.clear_all_cache()
+# try:
+#     logger.info('Бот запущен')
+#     bot.infinity_polling(skip_pending=True)
+# except KeyboardInterrupt:
+#     logger.info('Выход из программы')
+# finally:
+#     logger.warning('Программа остановлена')
+#     redis_cache.clear_all_cache()
 
-#
-# app = flask.Flask(__name__)
-# @app.route('/', methods=['POST'])
-# def webhook():
-#     """Обработка http-запросов, которые telegram пересылает на наш сервер"""
-#     if flask.request.headers.get('content-type') == 'application/json':
-#         json_string = flask.request.get_data().decode('utf-8')
-#         update = telebot.types.Update.de_json(json_string)
-#         bot.process_new_updates([update])
-#         return ''
-#     else:
-#         logger.error('abort(403)')
-#         flask.abort(403)
+
+app = flask.Flask(__name__)
+@app.route('/', methods=['POST'])
+def webhook():
+    """Обработка http-запросов, которые telegram пересылает на наш сервер"""
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        logger.error('abort(403)')
+        flask.abort(403)
 
